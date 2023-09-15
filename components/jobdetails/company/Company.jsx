c868721ed0641772c7c3eb5dc7aa3c00bb079d35
UTSAV MANDHANI <85448958+Utsav2929@users.noreply.github.com>
2023-09-15 11:05:56 +0530

    first-commit

---
+++ components/jobdetails/company/Company.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+// import styles from './company.style.js'
+
+const Company = () => {
+  return (
+    <View>
+      <Text>Company</Text>
+    </View>
+  )
+}
+
+export default Company
